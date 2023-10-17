dd60939e08fb65ad0f12b8502d62399a9c29acfb
ShokiYokota <syoki.yokota@azone-japan.co.jp>
2023-10-17 23:11:45 +0900

    減った割合ランキングに変更

--- app.js
+++ app.js
@@ -36,10 +36,12 @@ rl.on('close', () => {
     value.change = value.after / value.before
   }
   const rankingArray = Array.from(prefectureDataMap).sort((pair1, pair2) => {
-    return pair2[1].change - pair1[1].change
+    return pair1[1].change - pair2[1].change
   })
-  const rankigStrings = rankingArray.map(([key, value]) => {
-    return `${key}: ${value.before} => ${value.after} 変化率：${value.change}`
+  const rankigStrings = rankingArray.map(([key, value], index) => {
+    return `${index + 1}位 ${key}: ${value.before} => ${value.after} 変化率：${
+      value.change
+    }`
   })
   console.log(rankigStrings)
 })
