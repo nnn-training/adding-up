f14a7b4cb7547b62a039bb1a2ebcb648c5326f5a
isokaze <isokaze7@gmail.com>
2026-07-28 22:03:34 +0900

    ファイルの書き出しを追加

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 'use strict';
+const fs = require('node:fs');
+const readline = require('node:readline');
+const rs = fs.createReadStream('./popu-pref.csv');
+const rl = readline.createInterface({ input: rs });
+rl.on('line', lineString => {
+  const columns = lineString.split(',');
+  const year = parseInt(columns[0]);
+  const prefecture = columns[1];
+  const popu = parseInt(columns[3]);
+  if (year === 2016 || year === 2021){
+    console.log(year);
+    console.log(prefecture);
+    console.log(popu);
+  }
+
+});
